85e526c27fa67b7fed0183c8780cd9ae4d5a4b47
Isa <isaismail3000@gmail.com>
2025-11-30 19:57:46 +0300

    Create and add stars randomly on body

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+// function that gives random number between min and max inclusive
+function random(min, max) {
+  return min + Math.random() * (max + 1 - min);
+}
+
+// Work out number of total pixels on body and then decide what fraction of those should contain stars
+const body = document.querySelector("body");
+const canvasSize = body.offsetWidth * body.offsetHeight;
+const starsFraction = canvasSize / 2000;
+
+// loop that will add each star at a time
+// The xPos and yPos here will be percentages, hence needing a random number between 0 and 100.
+// Alpha is the opacity, which won't go below 0.5 because that will make it hard to see.
+// The size is between 1px and 2px, as anything smaller will be hard to see and anything bigger won't look as much like a star.
+
+for (let i = 0; i < starsFraction; i++) {
+  let xPos = random(0, 100);
+  let yPos = random(0, 100);
+  let alpha = random(0.5, 1);
+  let size = random(1, 2);
+  let color = "#ffffff";
+
+  // Add each star and use properties above to add styling
+
+  const star = document.createElement("div");
+  star.style.position = "relative";
+  star.style.left = xPos + "%";
+  star.style.top = yPos + "%";
+  star.style.opacity = alpha;
+  star.style.width = size + "px";
+  star.style.height = size + "px";
+  star.style.backgroundColor = color;
+  document.body.appendChild(star);
+}
